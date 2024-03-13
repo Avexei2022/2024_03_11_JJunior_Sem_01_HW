eb093de5d3ee4a8174f121ce97bab19ea2751316
alex123 <alex123@dot.com>
2024-03-13 22:49:55 +0500

    TaskToRun is changed: PersonsList is removed as argument

--- src/main/java/ru/gb/student/TasksToRun.java
+++ src/main/java/ru/gb/student/TasksToRun.java
@@ -16,19 +16,19 @@ public class TasksToRun {
     }
 
     public void run() {
-        Task_01(personsList);
-        Task_02_vol_01(personsList);
-        Task_02_vol_02(personsList);
-        Task_03(personsList);
-        Task_04(personsList);
+        Task_01();
+        Task_02_vol_01();
+        Task_02_vol_02();
+        Task_03();
+        Task_04();
     }
 
-    private void Task_01(PersonsList personsList){
+    private void Task_01(){
         System.out.println("\n Задача 1: Вывести на консоль отсортированные (по алфавиту) имена персонов.\n");
         homework.printNamesOrdered(personsList);
     }
 
-    private void Task_02_vol_01(PersonsList personsList) {
+    private void Task_02_vol_01() {
         System.out.println("\n Задача 2 Вариант 01: В каждом департаменте найти самого взрослого сотрудника.\n");
         long start = System.nanoTime();
         homework.printDepartmentOldestPerson01(personsList).forEach((k, v) -> System.out.println(k.getName()
@@ -37,7 +37,7 @@ public class TasksToRun {
         System.out.println("Время поиска: " + (end - start));
     }
 
-    private void Task_02_vol_02(PersonsList personsList) {
+    private void Task_02_vol_02() {
         System.out.println("\n Задача 2 Вариант 02: В каждом департаменте найти самого взрослого сотрудника.\n");
         long start = System.nanoTime();
         homework.printDepartmentOldestPerson02(personsList).forEach((k, v) -> System.out.println(k.getName()
@@ -46,12 +46,12 @@ public class TasksToRun {
         System.out.println("Время поиска: " + (end - start));
     }
 
-    private void Task_03(PersonsList personsList) {
+    private void Task_03() {
         System.out.println("\n Задача 3: Найти 10 первых сотрудников, младше 30 лет, у которых зарплата выше 50_000.\n");
         homework.findFirstPersons(personsList).forEach(System.out::println);
     }
 
-    private void Task_04(PersonsList personsList) {
+    private void Task_04() {
         System.out.println("\n Задача 4: Найти депаратмент, чья суммарная зарплата всех сотрудников максимальна.\n");
         System.out.println(homework.findTopDepartment(personsList).toString());
     }
